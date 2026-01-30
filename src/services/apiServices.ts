bff79af2a4adc713977aa8bc725e3a2646c1af3c
Kaique Lira <Kaiqueliira@gmail.com>
2026-01-30 12:26:47 -0300

    fix

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 import type { ApiError } from "@/types";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5057/api";
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 // Configuração do axios
 const api: AxiosInstance = axios.create({
